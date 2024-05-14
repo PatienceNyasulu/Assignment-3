e1853cffdc5e30bbeeb17590ebc652554b79933f
Czennie <83468952+PatienceNyasulu@users.noreply.github.com>
2024-05-14 15:14:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.markdown('[Link to GitHub code](https://github.com/PatienceNyasulu/Web_conten
 articles = pd.read_csv('clustered_articles.csv')
 
 # Get selected category
-selected_category = st.selectbox("Select a category:", ['politics', 'business', 'lifestyle', 'opinion & analysis'])
+selected_category = st.selectbox("Select a category:", ['politics', 'business', 'lifestyle', 'sport'])
 
 #st.write('Selected Category:', selected_category)
 
